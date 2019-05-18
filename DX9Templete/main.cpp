317e544e68b0846165dbcd55e9a70e24e30376a7
sartan8827 <sartan8827@gmail.com>
2019-05-18 16:17:01 +0900

    first commit

---
+++ DX9Templete/main.cpp
@@ -0,0 +1,137 @@
+﻿#define WIN32_LEAN_MEAN
+
+#include <windows.h>
+#include <tchar.h>
+#include <d3dx9.h>
+
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define SAFE_RELEASE(p) { if(p) { p->Release(); p = NULL; }}
+
+LPDIRECT3D9 pD3d;
+LPDIRECT3DDEVICE9 pDevice;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HRESULT InitD3d(HWND);
+void DrawSprite();
+void FreeDx();
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szStr, INT iCmdShow)
+{
+	HWND hWnd;
+	MSG msg;
+
+	LPCTSTR szAppName = "Direct3D Templete";
+	WNDCLASSEX wndClass;
+
+	wndClass.cbSize = sizeof(wndClass);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInstance;
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = szAppName;
+	wndClass.hIconSm = LoadIcon(NULL, IDI_ASTERISK);
+
+	RegisterClassEx(&wndClass);
+
+	hWnd = CreateWindow(szAppName, szAppName, WS_OVERLAPPEDWINDOW, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	SetWindowText(hWnd, "Direct3D Templete");
+
+	if (FAILED(InitD3d(hWnd)))
+	{
+		return 0;
+	}
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			DrawSprite();
+		}
+	}
+	FreeDx();
+	return (INT)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_KEYDOWN:
+		switch ((CHAR)wParam)
+		{
+		case VK_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		}
+	}
+	return DefWindowProc(hWnd, iMsg, wParam, lParam);
+}
+
+HRESULT InitD3d(HWND hWnd)
+{
+	pD3d = Direct3DCreate9(D3D_SDK_VERSION);
+	if (pD3d == NULL)
+	{
+		MessageBox(0, "DirectX3Dの作成に失敗しました", "", MB_OK);
+		return E_FAIL;
+	}
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.BackBufferCount = 1;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.Windowed = TRUE;
+
+	if (FAILED(pD3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &pDevice)))
+	{
+		if (FAILED(pD3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pDevice)))
+		{
+			if (FAILED(pD3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_REF, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &pDevice)))
+			{
+				if (FAILED(pD3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_REF, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pDevice)))
+				{
+					MessageBox(0, "Direct3Dデバイスの作成に失敗しました", "", MB_OK);
+					return E_FAIL;
+
+				}
+			}
+		}
+	}
+	return S_OK;
+}
+
+void DrawSprite()
+{
+	pDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+	pDevice->BeginScene();
+
+	pDevice->EndScene();
+	pDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+void FreeDx()
+{
+	SAFE_RELEASE(pDevice);
+	SAFE_RELEASE(pD3d);
+}
